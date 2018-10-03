627a27903fb3c72f1a15ddbb27533ecf9e510b0a
EC2 Default User <ec2-user@ip-172-31-7-171.us-east-2.compute.internal>
2018-10-03 15:12:26 +0000

    step13 complete

---
+++ lib/docking_station.rb
@@ -0,0 +1,39 @@
+# As a person,
+# So that I can use a bike,
+# I'd like a docking station to release a bike.
+
+# As a person,
+# So that I can use a good bike,
+# I'd like to see if a bike is workings
+
+
+# Objects | Messages
+
+# Person |
+# DockingStation | release_bike
+# Bike | is_working?
+
+# Diagram
+# Person -->  DockingStation --> release_bike --> Bike
+
+# bike <-- working? --> true/false
+
+# Error code:
+# NameError: uninitialized constant DockingStation
+# from (irb):1
+# from /Users/username/.rvm/rubies/ruby-2.2.2/bin/irb:11:in '<main>'
+require_relative 'bike'
+
+class DockingStation
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+  
+  def dock(bike)
+    fail 'Docking station full' if @bike
+    @bike = bike
+  end
+  
+ attr_reader :bike
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
-require 'docking station'
+require 'docking_station'
+require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  
+  describe '#dock' do
+  it 'raises an error when full' do
+  subject.dock(Bike.new)
+  expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+  end
+  end
 end
