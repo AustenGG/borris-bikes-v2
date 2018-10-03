207889ead63b0e25a17bd26b80a00573038664c1
EC2 Default User <ec2-user@ip-172-31-7-171.us-east-2.compute.internal>
2018-10-03 15:47:41 +0000

    Step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -25,15 +25,27 @@
 require_relative 'bike'
 
 class DockingStation
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
-  
+
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
-  
- attr_reader :bike
+
+private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 end
